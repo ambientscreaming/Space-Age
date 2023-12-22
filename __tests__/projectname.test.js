9baf1baaa4700e54f5df9da52861b9e794e0313a
Kelsey Burt <ambientscreaming@gmail.com>
2023-12-22 14:45:48 -0800

    initial test

---
+++ __tests__/projectname.test.js
@@ -0,0 +1,7 @@
+describe('SpaceAge', () => {
+
+  test('it creates an empty instance of the class', () => {
+    const spaceAge = new SpaceAge();
+    expect(spaceAge).toEqual(instanceof SpaceAge);
+  });
+});
