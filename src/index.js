6d5a99f42406295220946c19fc3bbf67534cf63b
Kelsey Burt <ambientscreaming@gmail.com>
2023-12-24 17:29:21 -0800

    change current planet function test pass

--- src/index.js
+++ src/index.js
@@ -2,4 +2,10 @@ export default class SpaceAge {
     constructor() {
         this.currentPlanet = "earth";
     }
+
+    changeCurrentPlanet(newPlanet) {
+        this.currentPlanet = newPlanet;
+    }
+
 }
+
