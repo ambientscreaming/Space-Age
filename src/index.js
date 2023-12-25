6605cd131c7f8add7cad4b34df25b7f9e93683e9
Kelsey Burt <ambientscreaming@gmail.com>
2023-12-24 21:20:54 -0800

    returns error message for undefined planet test pass

--- src/index.js
+++ src/index.js
@@ -22,6 +22,8 @@ export default class SpaceAge {
                 return (this.earthAge - pastBirthday) / .62;
             case 'mars':
                 return (this.earthAge - pastBirthday) / 1.88;
+            default:
+                return `that planet isn't in this solar system`;
         }
     }
 
